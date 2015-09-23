46b24a3ecabae5252310eff5d41db5ea96e07b20
Momo Ozawa <mozawa@uoregon.edu>
2015-09-23 10:07:05 -0700

    Pass mocha test for letting a player mark a space

--- spec/specs.js
+++ spec/specs.js
@@ -6,13 +6,20 @@ describe('Player', function() {
 });
 
 describe('Space', function() {
-  it("returns the player's x-coordinate", function() {
+  it("returns the space's x-coordinate", function() {
     var testSpace = new Space(1,2);
     expect(testSpace.xCoordinate).to.equal(1);
   });
 
-  it("returns the player's y-coordinate", function() {
+  it("returns the space's y-coordinate", function() {
     var testSpace = new Space(1,2);
     expect(testSpace.yCoordinate).to.equal(2);
+  });
+
+  it("lets a player mark a space", function() {
+    var testPlayer = new Player('X');
+    var testSpace = new Space(1,2);
+    testSpace.takenBy(testPlayer);
+    expect(testSpace.markedBy).to.eql(testPlayer);
   });
 });
