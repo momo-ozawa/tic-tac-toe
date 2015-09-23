5a5e01577519721f7a90920be206b2af8766de8b
Momo Ozawa <mozawa@uoregon.edu>
2015-09-23 10:04:32 -0700

    Pass mocha test for Space constructor

--- spec/specs.js
+++ spec/specs.js
@@ -4,3 +4,15 @@ describe('Player', function() {
     expect(testPlayer.mark).to.equal('X');
   });
 });
+
+describe('Space', function() {
+  it("returns the player's x-coordinate", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.xCoordinate).to.equal(1);
+  });
+
+  it("returns the player's y-coordinate", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.yCoordinate).to.equal(2);
+  });
+});
