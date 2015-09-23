bcfbaecf2aadfdce263d4330e1705f0d67bbb540
Momo Ozawa <mozawa@uoregon.edu>
2015-09-23 16:54:43 -0700

    Add game logic

--- js/user-interface.js
+++ js/user-interface.js
@@ -2,6 +2,11 @@
  * JAVASCRIPT
  */
 
+function convertValueToIntArray(value) {
+  return value.split('').map(
+    function(n) { return parseInt(n) }
+  );
+}
 
 /**
  * JQUERY
@@ -14,8 +19,20 @@ $(document).ready(function() {
 
   $('.square').click(function() {
     if (!($(this).hasClass('marked'))) {
+      // Mark space in game.board as taken by current player
+      var selectedSquare = $('this').attr('id');
+      console.log(selectedSquare);
+      // var coordinates = convertValueToIntArray(selectedSquare);
+      // var xCoordinate = coordinates[0];
+      // var yCoordinate = coordinates[1];
+      // game.board.find(xCoordinate, yCoordinate).takenBy(game.currentPlayer);
+      // console.log(game.board.groups());
+
+      // Mark a square on the web app
       $(this).text(game.currentPlayer.mark);
       $(this).addClass('marked');
+
+      // Switch player
       game.switchPlayer();
     }
   });
