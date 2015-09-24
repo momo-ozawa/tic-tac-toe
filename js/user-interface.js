a6309d59a85d57930187404cb573ff687fc28d53
Momo Ozawa <mozawa@uoregon.edu>
2015-09-23 23:37:53 -0700

    DRY up code

--- js/user-interface.js
+++ js/user-interface.js
@@ -2,17 +2,48 @@
  * JAVASCRIPT
  */
 
+function showWhichPlayerGoesFirst(currentGame) {
+  $('span#commentary').text(currentGame.currentPlayer.mark + ' goes first this round.').show();
+}
+
+function fadeOutWhichPlayerGoesFirst() {
+  $('span#commentary').fadeOut();
+}
+
+function markSpaceAsTaken(currentGame, squareId) {
+  var coordinates = convertValueToIntArray(squareId);
+  var xCoordinate = coordinates[0];
+  var yCoordinate = coordinates[1];
+  currentGame.board.find(xCoordinate, yCoordinate).takenBy(currentGame.currentPlayer);
+  console.log(currentGame.board.groups());
+}
+
 function convertValueToIntArray(value) {
   return value.split('').map(
     function(n) { return parseInt(n) }
   );
 }
 
+function gameOverActions(currentGame) {
+  alert(currentGame.gameOverMessage());
+
+  // Update scores
+  $('span#player1score').text(currentGame.player1.score);
+  $('span#player2score').text(currentGame.player2.score);
+  
+  // Clear board in web app
+  clearBoard();
+
+  // Start new round
+  currentGame.newRound();
+  showWhichPlayerGoesFirst(currentGame);
+}
 
 function clearBoard() {
   $('.square').removeClass('marked');
   $('.square').text('');
 }
+
 
 /**
  * JQUERY
@@ -21,36 +52,28 @@ function clearBoard() {
 $(document).ready(function() {
 
   // Initialize a new game
-  var game = new Game();
+  var myGame = new Game();
+  showWhichPlayerGoesFirst(myGame);
 
   $('.square').click(function() {
+    fadeOutWhichPlayerGoesFirst();
     if (!($(this).hasClass('marked'))) {
+      // Grab selected square's id
+      var selectedSquareId = $(this).attr('id');
+      console.log(selectedSquareId);
+     
       // Mark space in game.board as taken by current player
-      var selectedSquare = $(this).attr('id');
-      var coordinates = convertValueToIntArray(selectedSquare);
-      var xCoordinate = coordinates[0];
-      var yCoordinate = coordinates[1];
-      game.board.find(xCoordinate, yCoordinate).takenBy(game.currentPlayer);
-      console.log(game.board.groups());
+      markSpaceAsTaken(myGame, selectedSquareId);
 
       // Mark a square on the web app
-      $(this).text(game.currentPlayer.mark);
+      $(this).text(myGame.currentPlayer.mark);
       $(this).addClass('marked');
 
       // Check if game is over; if not, switch player
-      if (game.isGameOver()) {
-        alert(game.gameOverMessage());
-        $('span#player1score').text(game.player1.score);
-        $('span#player2score').text(game.player2.score);
-        
-        // Clear board in web app
-        clearBoard();
-
-        // Start new round
-        game.newRound();
-
+      if (myGame.isGameOver()) {
+        gameOverActions(myGame);
       } else {
-        game.switchPlayer();
+        myGame.switchPlayer();
       }
     }
   });
